bfa37435bdf623edd7d2a6921075acc3bb1f0516
z-Wolle <daniel.zimmermann@stud.thga.de>
2022-02-03 10:46:42 +0100

    Hier wurden Kommentare in die main.cpp  hinzugefügt

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include<QtGui>
 
 
-class MainWindow : public QWidget
+class MainWindow :  public QWidget
 {
   public:
   MainWindow();
@@ -14,12 +14,15 @@ MainWindow::MainWindow()
   setMinimumSize(1000, 600);
 
    //LineEdit_1, Spin, PushButton, slider, Progressbar, Spinbox
-
   QLineEdit *line_1 = new QLineEdit();
   line_1->setText("Zahlen von 0-999 können multipliziert werden!");
   line_1->setMaximumSize(300,35);
 
-  //berechnen SpinBoxen!
+
+  //Hier werden Die SpinBoxen erstellt!
+  QSpinBox *spin=new QSpinBox();
+  spin->setRange(0,100);
+
   QSpinBox *spin_2 = new QSpinBox();
   spin_2->setRange(0,999);
   
@@ -29,30 +32,32 @@ MainWindow::MainWindow()
   QSpinBox *spin_4 = new QSpinBox();
   spin_4->setRange(0,999999);
 
- //PushButton
+ //Hier werden die PushButton erstellt!
   QPushButton *pb_1 = new QPushButton();
   pb_1->setText("close");
 
   QPushButton *pb_2 = new QPushButton();
   pb_2->setText("Brechne");
  
+ //Hier wird der Slider erstellt!
   QSlider *slider =new QSlider(Qt::Orientation::Horizontal);
   slider->setTickPosition(QSlider::TicksBothSides);
   slider->setTickInterval(10);
   slider->setSingleStep(1);
   slider->setRange(0,100);
   
+  //Hier wird der ProgressBar erstellt!
   QProgressBar *Bar= new QProgressBar();
   Bar->setRange(0,100);
-
-  QSpinBox *spin=new QSpinBox();
-  spin->setRange(0,100);
   
+  //Hier wird der die "font" für die spinbox erstellt sodass die größer 
+  // und ansprechender aussieht.
   QFont font = spin->font();
   font.setPointSize(15);
   spin->setFont(font);
 
   //Layout definition
+  //###################################################################################
 	QHBoxLayout *layout1 = new QHBoxLayout();
 	layout1->addStretch();
   layout1->addWidget(slider);
@@ -72,6 +77,7 @@ MainWindow::MainWindow()
   layout4->addStretch();
   layout4->addWidget(pb_1);
   layout4->addStretch();
+
   //die lineEdite
   QHBoxLayout *layout5 = new QHBoxLayout();
 	layout5->addStretch();
@@ -88,6 +94,7 @@ MainWindow::MainWindow()
   layout6->addWidget(spin_3);
   layout6->addStretch();
 
+//Hier fängt die MainBox an! 
   QVBoxLayout *MainBox = new QVBoxLayout(this);
   MainBox->addStretch();
   MainBox->addLayout(layout1);
@@ -101,9 +108,10 @@ MainWindow::MainWindow()
   MainBox->addLayout(layout6);
   MainBox->addSpacing(50);
   MainBox->addLayout(layout4);
-  
 
-//PushButton
+//hier folgen die Connect Befehle
+//###################################################################################
+//PushButton schließt das 
   QObject::connect(pb_1, &QPushButton::clicked, this 
     ,&QWidget::close);
 //Slider,Bar, spin
@@ -115,7 +123,7 @@ MainWindow::MainWindow()
 
   QObject::connect(spin, cs_mp_cast<int>(&QSpinBox::valueChanged), slider 
     ,&QSlider::setValue);
-
+//Hier folgt die verknüpfung zwischen dem "berechne " 
   QObject::connect(pb_2,&QPushButton::clicked, spin_4, [spin_2,spin_3,spin_4] () {
     spin_4->setValue((spin_2->value())*(spin_3->value()));
   });
